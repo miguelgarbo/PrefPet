ffbd8080fabe645b3d9a5015dd64d783e4a523cf
miguelgarbo <miguelggarbo@gmail.com>
2025-08-07 21:03:40 -0300

    Pastas Add

---
+++ src/main/java/com/uni/PrefPet/PrefPetApplication.java
@@ -0,0 +1,13 @@
+package com.uni.PrefPet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrefPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrefPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uni/PrefPet/PrefPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uni.PrefPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrefPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
