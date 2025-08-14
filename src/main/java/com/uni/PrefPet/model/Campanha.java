7bf005fad4cc1cce6f0745d13c8bb8ab7d53c036
João Pedro Reis Felini <joaoreisfelini@gmail.com>
2025-08-13 21:00:54 -0300

    ADD: Desenvolvido campanha model

--- src/main/java/com/uni/PrefPet/model/Campanha.java
+++ src/main/java/com/uni/PrefPet/model/Campanha.java
@@ -1,7 +1,24 @@
 package com.uni.PrefPet.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+
 public class Campanha {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private LocalDate dataCriacao;
 
-
-
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario escritor;
 }
