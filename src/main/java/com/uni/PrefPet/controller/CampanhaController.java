72bd870046ef7ad0a105bd7ca22a736712124e12
Pablo Centurion <pablojunior145@hotmail.com>
2025-08-16 17:25:44 -0300

    Campanha completa com as validações.

--- src/main/java/com/uni/PrefPet/controller/CampanhaController.java
+++ src/main/java/com/uni/PrefPet/controller/CampanhaController.java
@@ -2,7 +2,9 @@ package com.uni.PrefPet.controller;
 
 import com.uni.PrefPet.model.Campanha;
 import com.uni.PrefPet.service.CampanhaService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,28 +18,52 @@ public class CampanhaController {
     private CampanhaService campanhaService;
 
     @PostMapping("/save")
-    public ResponseEntity<Campanha> save(@RequestBody Campanha campanha) {
-        Campanha novaCampanha = campanhaService.save(campanha);
-        return ResponseEntity.ok(novaCampanha);
+    public ResponseEntity<Campanha> save(@Valid @RequestBody Campanha campanha) {
+        try {
+            var novaCampanha = campanhaService.save(campanha);
+            return new ResponseEntity<>(novaCampanha, HttpStatus.CREATED);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/findById/{id}")
     public ResponseEntity<Campanha> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(campanhaService.findById(id));
+        try {
+            var result = campanhaService.findById(id);
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/listAll")
     public ResponseEntity<List<Campanha>> listAll() {
-        return ResponseEntity.ok(campanhaService.findAll());
+        try {
+            var result = campanhaService.findAll();
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @PutMapping("/update/{id}")
-    public ResponseEntity<Campanha> update(@PathVariable Long id, @RequestBody Campanha campanha) {
-        return ResponseEntity.ok(campanhaService.update(id, campanha));
+    public ResponseEntity<Campanha> update(@PathVariable Long id, @Valid @RequestBody Campanha campanha) {
+        try {
+            var updatedCampanha = campanhaService.update(id, campanha);
+            return new ResponseEntity<>(updatedCampanha, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> delete(@PathVariable Long id) {
-        return ResponseEntity.ok(campanhaService.delete(id));
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        try {
+            campanhaService.delete(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
